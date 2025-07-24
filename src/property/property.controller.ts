fb0b6a474d594a5b8589db5ebea56d5bf80ce3b5
HARSHANA D.M.S <121012829+Sandun-COM-80@users.noreply.github.com>
2025-07-24 23:10:31 +0530

    update 2

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -8,12 +8,17 @@ export class PropertyController {
         return 'all properties';
     }
 
+    @Get(':id/:slug')
+    findOne(@Param('id') id: string, @Param('slug') slug: string) {
+        return `id = ${id}, slug = ${slug}`;
+    }
+
     @Post()
     create(): string {
         return 'create property';
     }
 
-    
+
 }
 
 
